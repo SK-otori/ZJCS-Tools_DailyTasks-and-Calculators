de1fef8c136caeaf1b9f18cf013282f92b625444
SK-otori <qq992204711@gmail.com>
2026-06-21 16:51:48 +0800

    版本号升级

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.otori.zjcstools"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.2.0"
+        versionCode = 5
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
